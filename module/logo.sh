3b0ad0599b4655f5a4d24d3eb18b4e0c33b7d340
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 10:11:26 +0700

    Create logo.sh

---
+++ module/logo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#install project
+figlet Install Bahan | lolcat
+sleep 3
+
+pkg update && pkg upgrade
+pkg install git
+
+
+figlet selesai | lolcat
+sleep 1
+
+local spinner=('●▪▪▪' '▪●▪▪' '▪▪●▪' '▪▪▪●' )
+sleep 5
+
+# tampilan
+clear
+ local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
+ sleep 5
+toilet -f big -F gay 
+echo "=============================================" | lolcat
+echo "[•] Author : MR-Xyaa                         "
+echo "[•] FB     : Xyaa Xyaa                         "
+echo "[•] GitHub : github.com/MR-Xyaa              "
+echo "=============================================" | lolcat
+
+
